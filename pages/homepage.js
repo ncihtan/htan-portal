385ddda7c67a01f87b998d44bce25c344d551a9f
esiegel <esiegel@jimmy.harvard.edu>
2020-03-10 11:59:48 -0400

    add placeholder cards

--- pages/homepage.js
+++ pages/homepage.js
@@ -3,28 +3,162 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 const Homepage = () => (
     <React.Fragment>
         <Row className="justify-content-md-center mt-5">
             <h1>Human Tumor Atlas Network Data Portal</h1>
         </Row>
+
         <Row className="justify-content-md-center mt-5">
             <Col md={{span: 4}}>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat.
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
                 </p>
             </Col>
         </Row>
-        <Row className="justify-content-md-center">
+
+        <Row className="justify-content-md-center mt-3">
             <ButtonToolbar>
                 <Button href="#" variant="primary" className="mr-4">Explore the Data</Button>
                 <Button variant="secondary">Learn More</Button>
             </ButtonToolbar>
+        </Row>
+
+        <Row className="justify-content-md-center mt-5">
+            <Col md={{span: 8}}>
+                <CardGroup>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </Col>
+        </Row>
+
+        <Row className="justify-content-md-center mt-5">
+            <Col md={{span: 8}}>
+                <CardGroup>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </Col>
+        </Row>
+
+        <Row className="justify-content-md-center mt-5">
+            <Col md={{span: 8}}>
+                <CardGroup>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Some more text</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </Col>
         </Row>
     </React.Fragment>
 )
