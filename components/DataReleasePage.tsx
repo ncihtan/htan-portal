42d6bb507815c7a68e4e0f3f693d9541e5d358ee
ethansiegl <09esiegel@gmail.com>
2020-03-17 16:04:21 -0400

    data release summary blurb API call

--- components/DataReleasePage.tsx
+++ components/DataReleasePage.tsx
@@ -7,12 +7,13 @@ import Breadcrumb from "react-bootstrap/Breadcrumb";
 import _ from 'lodash'
 
 import HtanNavbar from "./HtanNavbar";
+import {getContent} from "../ApiUtil";
 
 
 /**
  * Strip HTML characters and get content from CMS
  */
-function getContent(data: CmsData[], slug: string): string {
+function cleanContent(data: CmsData[], slug: string): string {
     let content: string | undefined;
 
     const filteredData = _.filter(data, d => d.slug === slug);
@@ -43,172 +44,164 @@ export interface DataReleaseProps {
     data: CmsData[];
 }
 
-const DataReleasePage = (props: DataReleaseProps) => (
-    <>
-        <HtanNavbar/>
-        <Container>
-            <Row>
-                <Breadcrumb className="mt-3">
-                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                    <Breadcrumb.Item active>
-                        Data Release
-                    </Breadcrumb.Item>
-                </Breadcrumb>
-            </Row>
+const DataReleasePage = (props: DataReleaseProps) => {
+    let summaryContent = getContent("data-release", "summary-blurb")
+    return (
+        <>
+            <HtanNavbar/>
+            <Container>
+                <Row>
+                    <Breadcrumb className="mt-3">
+                        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                        <Breadcrumb.Item active>
+                            Data Release
+                        </Breadcrumb.Item>
+                    </Breadcrumb>
+                </Row>
 
-            <Row className="mt-3">
-                <h1>Data Release</h1>
-            </Row>
+                <Row className="mt-3">
+                    <h1>Data Release</h1>
+                </Row>
 
-            <Row className="mt-3">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id est
-                    laborum.
-                </p>
-            </Row>
+                <Row className="mt-3">
+                    <span dangerouslySetInnerHTML={{__html: summaryContent}} />
+                </Row>
 
-            <Row className="mt-3">
-                <Table>
-                    <thead>
-                    <tr>
-                        <th>Atlas</th>
-                        <th>Data Release</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta1-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta1">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta2-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta2">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta3-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta3">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta4-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta4">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta5-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta5">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta6-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta6">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta7-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta7">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta8-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta8">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta9-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta9">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta10-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta10">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta11-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta11">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            {getContent(props.data, 'hta12-short-blurb')}
-                        </td>
-                        <td>
-                            <Link href="/data/hta12">
-                                <a>Data Release</a>
-                            </Link>
-                        </td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </Row>
-        </Container>
-    </>
-);
+                <Row className="mt-3">
+                    <Table>
+                        <thead>
+                        <tr>
+                            <th>Atlas</th>
+                            <th>Data Release</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta1-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta1">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta2-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta2">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta3-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta3">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta4-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta4">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta5-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta5">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta6-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta6">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta7-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta7">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta8-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta8">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta9-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta9">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta10-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta10">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta11-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta11">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {cleanContent(props.data, 'hta12-short-blurb')}
+                            </td>
+                            <td>
+                                <Link href="/data/hta12">
+                                    <a>Data Release</a>
+                                </Link>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                </Row>
+            </Container>
+        </>
+    );
+}
 
 export default DataReleasePage;
