1348ccefff2d627faba3a247c732c1bb643ea0cd
esiegel <esiegel@jimmy.harvard.edu>
2020-03-30 17:14:03 -0400

    move standards subpages to server API calls

--- pages/standards/biospecimens.tsx
+++ pages/standards/biospecimens.tsx
@@ -4,10 +4,10 @@ import Footer from "../../components/Footer";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
-import {getContent} from "../../ApiUtil";
+import {GetServerSideProps} from "next";
+import fetch from "node-fetch"
 
-function Biospecimens() {
-    const content = getContent("cds-blurb","data-standards");
+function Biospecimens(data: any) {
     return (
         <>
             <HtanNavbar/>
@@ -22,12 +22,20 @@ function Biospecimens() {
                     </Breadcrumb>
                 </Row>
                 <Row>
-                    <span dangerouslySetInnerHTML={{__html: content}}></span>
+                    <span dangerouslySetInnerHTML={{__html: data.data[0].content.rendered}}></span>
                 </Row>
             </Container>
             <Footer/>
         </>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+    let slugs = ["data-standards-biospecimen-blurb"];
+    let overviewURL = `https://humantumoratlas.org/wp-json/wp/v2/pages/?slug=${JSON.stringify(slugs)}&_fields=content,slug,title`;
+    let res = await fetch(overviewURL);
+    let data = await res.json();
+    return {props: {data}}
 }
 
 export default Biospecimens;
--- pages/standards/rnaseq.tsx
+++ pages/standards/rnaseq.tsx
@@ -4,10 +4,10 @@ import Footer from "../../components/Footer";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
-import {getContent} from "../../ApiUtil";
+import {GetServerSideProps} from "next";
+import fetch from "node-fetch";
 
-function Rnaseq() {
-    const content = getContent("rnaseq-blurb","data-standards");
+function Rnaseq(data: any) {
     return (
         <>
             <HtanNavbar/>
@@ -22,12 +22,20 @@ function Rnaseq() {
                     </Breadcrumb>
                 </Row>
                 <Row>
-                    <span dangerouslySetInnerHTML={{__html: content}}></span>
+                    <span dangerouslySetInnerHTML={{__html: data.data[0].content.rendered}}></span>
                 </Row>
             </Container>
             <Footer/>
         </>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+    let slugs = ["data-standards-rnaseq-blurb"];
+    let overviewURL = `https://humantumoratlas.org/wp-json/wp/v2/pages/?slug=${JSON.stringify(slugs)}&_fields=content,slug,title`;
+    let res = await fetch(overviewURL);
+    let data = await res.json();
+    return {props: {data}}
 }
 
 export default Rnaseq;
