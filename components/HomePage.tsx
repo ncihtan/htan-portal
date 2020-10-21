2eb61b5dc1da9ff3623f8a8105251d3142f5668b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2020-10-21 18:05:55 -0400

    Add dashboard numbers on homepage

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -13,10 +13,33 @@ export interface IHomePropsProps {
     cards: any[];
 }
 
+function dashboardIcon(text:string, description:string) {
+    return (
+        <Col key={`icon-${description}`} xs lg="2" >
+            <div style={{textAlign:"center"}}>
+                <div style={{color:"white",fontSize:"60px", lineHeight:"70px"}}>
+                    {text}
+                </div>
+                <div style={{color:"white",fontSize:"20px"}}>
+                    {description}
+                </div>
+            </div>
+        </Col>
+    );
+}
+
+
 const HomePage: React.FunctionComponent<IHomePropsProps> = ({ hero_blurb, cards}) => {
 
+    const dashboardData = [
+        {text: "12", description: "Atlases"},
+        {text: "11", description: "Organs"},
+        {text: ">1K", description: "Participants"},
+        {text: ">10K", description: "Biospecimens"},
+    ];
+
     return (<>
-        <Jumbotron className={"text-center"}>
+        <Jumbotron className={"text-center"} style={{marginBottom:"0px"}}>
             <Row className="justify-content-md-center">
                 <h1>Human Tumor Atlas Network Data Portal</h1>
             </Row>
@@ -35,6 +58,11 @@ const HomePage: React.FunctionComponent<IHomePropsProps> = ({ hero_blurb, cards}
                 </ButtonToolbar>
             </Row>
         </Jumbotron>
+    <Container fluid style={{backgroundColor:"#6B618B",paddingTop:"60px",paddingBottom:"60px"}}>
+        <Row className="justify-content-md-center" >
+            {dashboardData.map((icon) => dashboardIcon(icon.text, icon.description))}
+        </Row>
+    </Container>
     <Container>
         <Row className="justify-content-md-center mt-5">
             <Col md={{span: 8}}>
