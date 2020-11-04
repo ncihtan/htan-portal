e64a96997519465291cfe37e6c549650071e36c7
Aaron Lisman <aaronlisman@Aarons-MacBook-Pro.local>
2020-11-03 22:22:58 -0500

    filters

--- pages/test.tsx
+++ pages/test.tsx
@@ -10,23 +10,59 @@ import {getContent, WORDPRESS_BASE_URL} from "../ApiUtil";
 import _ from 'lodash';
 import {doIt, entity} from "./lib/helpers";
 
+enum PropNames {
+    TissueorOrganofOrigin = "TissueorOrganofOrigin",
+    PrimaryDiagnosis = "PrimaryDiagnosis",
+    Component = "Component"
+}
 
+const propMap = {
+    [PropNames.TissueorOrganofOrigin]: {
+        prop: "diagnosis.TissueorOrganofOrigin"
+    }, 
+    [PropNames.PrimaryDiagnosis]: {
+        prop: "diagnosis.PrimaryDiagnosis"
+    }, 
+    [PropNames.Component]: {
+        prop: "Component"
+    }   
+}
 
-
-class Moo extends React.Component<{},{files:entity[], selectedOrgan:string|undefined}> {
+class Moo extends React.Component<{},{files:entity[], selectedDiagnsis:string|undefined, selectedOrgan:string|undefined}> {
 
     constructor(props){
         super(props);
-        this.state = { files:[], selectedOrgan:undefined };
+        this.state = { files:[], filters:{}, selectedOrgan:undefined };
     }
 
-    get organTypes() {
-        return _(this.state.files).map((f)=>f.diagnosis.TissueorOrganofOrigin).uniq().value()
+    get getGroupsByProperty(){
+        const m = {};
+        _.forEach(propMap, (o, k)=>{
+            console.log(o.prop);
+            m[k] = _.groupBy(this.state.files,(f)=>{
+                const val  = _.at(f, [o.prop]);
+                return val ? val[0] : "other"; 
+            });
+        });
+        return m;
+    }
+
+    setFilter(name, val){
+        console.log("filters", name);
+        const filters = Object.assign({}, this.state.filters);
+        if (val) {
+            filters[name] = val;
+        } else {
+            delete filters[name];
+        }
+        this.setState({filters:filters});
     }
 
     componentDidMount(): void {
 
         const data = doIt().then((files)=>{
+
+            console.log(files);
 
             const filteredFiles = files.filter((f)=>!!f.diagnosis);
 
@@ -35,8 +71,18 @@ class Moo extends React.Component<{},{files:entity[], selectedOrgan:string|undef
     }
 
     get filteredFiles(){
-        if (this.state.selectedOrgan) {
-            return this.state.files.filter((f)=>f.diagnosis.TissueorOrganofOrigin === this.state.selectedOrgan);
+        // if (this.state.selectedOrgan) {
+        //     return this.state.files.filter((f)=>f.diagnosis.TissueorOrganofOrigin === this.state.selectedOrgan);
+        // } else {
+        //     return this.state.files;
+        // }
+        if (_.size(this.state.filters)) {
+            return this.state.files.filter((f)=>{
+                return _.every(this.state.filters,(filter, name)=>{
+                    const val = _.at(f,propMap[name].prop);
+                    return val ? val[0] === filter : false;
+                });
+            });
         } else {
             return this.state.files;
         }
@@ -44,17 +90,71 @@ class Moo extends React.Component<{},{files:entity[], selectedOrgan:string|undef
 
     render(){
 
+        var self = this;
+
+        console.log("filter", this.state.filters);
+
+
         if (this.filteredFiles) {
 
-
             return <div style={{padding:20}}>
-                <select className="form-control"  style={{marginBottom:20}} onChange={function(e) {
-                    this.setState({selectedOrgan: e.target.value})
-                }.bind(this)}>
-                    {
-                        this.organTypes.map((organ)=><option>{organ}</option>)
-                    }
-                </select>
+                
+                <div className={'summary'}>
+                   
+                    <div>
+                        {this.state.files.length} Files
+                    </div>
+
+                    <div>
+                        {_.keys(this.getGroupsByProperty[PropNames.TissueorOrganofOrigin]).length} Organs
+                    </div>
+
+                    <div>
+                        {_.keys(this.getGroupsByProperty[PropNames.PrimaryDiagnosis]).length} Cancer Types
+                    </div>
+
+                    <div>
+                        {_.keys(this.getGroupsByProperty[PropNames.Component]).length} Assays
+                    </div>
+
+                </div>
+
+
+                <div className="filterControls">
+                    
+                    <select className="form-control" placeholder="helo"  style={{marginBottom:20, maxWidth:200}} onChange={function(e) {
+                        self.setFilter(PropNames.TissueorOrganofOrigin, e.target.value)
+                    }.bind(this)}>
+                        {
+                            _.map(this.getGroupsByProperty[PropNames.TissueorOrganofOrigin],(val, key)=>{
+                            return <option value={key}>{key} ({val.length})</option>
+                            })
+                        }
+                    </select>
+
+                    <select className="form-control"  style={{marginBottom:20, maxWidth:200}} onChange={function(e) {
+                        self.setFilter(PropNames.PrimaryDiagnosis, e.target.value);
+                        //this.setState({selectedDiagnosis: e.target.value})
+                    }}>
+                        {
+                            _.map(this.getGroupsByProperty[PropNames.PrimaryDiagnosis],(val, key)=>{
+                            return <option value={key}>{key} ({val.length})</option>
+                            })
+                        }
+                    </select>
+
+                    <select className="form-control"  style={{marginBottom:20, maxWidth:200}} onChange={function(e) {
+                        self.setFilter(PropNames.Component, e.target.value);
+                        //this.setState({selectedDiagnosis: e.target.value})
+                    }}>
+                        {
+                            _.map(this.getGroupsByProperty[PropNames.Component],(val, key)=>{
+                            return <option value={key}>{key.replace(/^bts:/,"")} ({val.length})</option>
+                            })
+                        }
+                    </select>        
+
+                </div>
 
                 <ResultTable entities={this.filteredFiles}>
                 </ResultTable>
@@ -106,8 +206,10 @@ class ResultTable extends React.Component<{entities:entity[]},{filteredEntitites
                 <tr>
                     <th>Filename</th>
                     <th>Atlas</th>
+                    <th>Assay</th>
                     <th>Organ</th>
-                    <th>Filename</th>
+                    <th>Diagnosis</th>
+                   
                 </tr>
                 </thead>
                 <tbody>
@@ -116,8 +218,9 @@ class ResultTable extends React.Component<{entities:entity[]},{filteredEntitites
                         return <tr>
                             <td>{file.filename}</td>
                             <td>{file.atlasid}</td>
+                            <td>{file.Component.replace(/^bts:/,"")}</td>
                             <td>{ file.diagnosis && file.diagnosis.TissueorOrganofOrigin}</td>
-                            <td>{file.atlasid}</td>
+                            <td>{ file.diagnosis && file.diagnosis.PrimaryDiagnosis}</td>
                         </tr>
                     })
                 }
