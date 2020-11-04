5eb3cb63198ed4080ce2fa81143c73349006d48d
Aaron Lisman <aaronlisman@Aarons-MacBook-Pro.local>
2020-11-03 22:26:21 -0500

    stuff

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,24 +2,6 @@ import fetch from 'node-fetch';
 import React from "react";
 
 import {DataReleasePage, DataReleaseProps} from "../components/DataReleasePage";
-import {getAtlasList, WORDPRESS_BASE_URL} from "../ApiUtil";
-import { GetStaticProps} from "next";
-
-export const getStaticProps: GetStaticProps = async context => {
-    // let slugs = ["summary-blurb-data-release"];
-    // let overviewURL = `${WORDPRESS_BASE_URL}${JSON.stringify(slugs)}`;
-    // let res = await fetch(overviewURL);
-    // let data = await res.json();
-
-    // const atlases = await getAtlasList();
-
-    // return {
-    //     props: {
-    //         atlasData:atlases,
-    //         data
-    //     }
-    // }
-}
 
 function DataRelease(props: DataReleaseProps) {
     return <DataReleasePage {...props} />;
