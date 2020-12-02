f3c1c0d7fe9df2c81b59db8b9a749e54a5e77b1a
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2020-12-01 19:18:02 -0500

    Remove all pages except for the Data Standards

--- components/HtanNavbar.tsx
+++ components/HtanNavbar.tsx
@@ -26,10 +26,7 @@ const HtanNavbar = () => (
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="/explore">Explore</Nav.Link>
-        <Nav.Link href="/standards">Data Standards</Nav.Link>
-        <Nav.Link href="/transfer">Data Transfer</Nav.Link>
-        <Nav.Link href="/tools">Analysis Tools</Nav.Link>
+        <Nav.Link href="/">Data Standards</Nav.Link>
       </Nav>
     </Navbar.Collapse>
     <Nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,77 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 import HtanNavbar from "../components/HtanNavbar";
-import HomePage, { IHomePropsProps } from "../components/HomePage";
 import Footer from "../components/Footer";
-import { GetStaticProps } from "next";
+import Link from "next/link";
+import { GetServerSideProps, GetStaticProps } from "next";
 import fetch from "node-fetch";
-import { WPConstants } from "../types";
-import { getContent, WORDPRESS_BASE_URL } from "../ApiUtil";
+import { CmsData } from "../types";
+import { WORDPRESS_BASE_URL } from "../ApiUtil";
 
-const Home = (data: IHomePropsProps) => {
+export interface StandardsProps {
+  data: CmsData[];
+}
+
+const Standards = (data: StandardsProps) => {
   return (
     <>
       <HtanNavbar />
-      <HomePage {...data} />
+      <Container>
+        <Row className="mt-3">
+          <h1>Data Standards</h1>
+        </Row>
+        <Row className="mt-3">
+          <span
+            dangerouslySetInnerHTML={{ __html: data.data[0].content.rendered }}
+          />
+        </Row>
+        <Row className="mt-3">
+          <h4>Browse Standards</h4>
+        </Row>
+        <Row>
+          <ul>
+            <li>
+              <Link href="/standard/design">Design Principles</Link>
+            </li>
+            <li>
+              <Link href="/standard/clinical">Clinical Data</Link>
+            </li>
+            <li>
+              <Link href="/standard/biospecimen">Biospecimen</Link>
+            </li>
+            <li>
+              <Link href="/standard/imaging">Imaging</Link>
+            </li>
+            <li>
+              <Link href="/standard/rnaseq">
+                Single Cell and Single Nucleus RNA Seq (sc/snRNASeq)
+              </Link>
+            </li>
+            <li>
+              <Link href="/standard/scatacseq">Single Cell ATAC Seq</Link>
+            </li>
+            <li>
+              <Link href="/standard/bulkrnaseq">Bulk RNA Seq</Link>
+            </li>
+            <li>
+              <Link href="/standard/bulkdnaseq">Bulk DNA Seq</Link>
+            </li>
+          </ul>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  let slugs = [WPConstants.HOMEPAGE_HERO_BLURB];
-
+  let slugs = ["summary-blurb-data-standards"];
   let overviewURL = `${WORDPRESS_BASE_URL}${JSON.stringify(slugs)}`;
   let res = await fetch(overviewURL);
   let data = await res.json();
-
-  const homepageContent = data.find(
-    (d: any) => d.slug === WPConstants.HOMEPAGE_HERO_BLURB
-  );
-
-  const cards = await Promise.all([
-    getContent("card-1", "homepage"),
-    getContent("card-2", "homepage"),
-    getContent("card-3", "homepage"),
-    getContent("card-4", "homepage"),
-    getContent("card-5", "homepage"),
-    getContent("card-6", "homepage"),
-  ]);
-
-  return {
-    props: { hero_blurb: homepageContent.content.rendered, cards: cards },
-  };
+  return { props: { data } };
 };
 
-export default Home;
+export default Standards;
