306c1be06e50e5162a3c5ab5d98ed7ef64b0e94e
Adam Abeshouse <abeshoua@mskcc.org>
2021-02-04 20:36:18 -0500

    Add filters for file format
    Signed-off-by: Adam Abeshouse <abeshoua@mskcc.org>

--- lib/types.ts
+++ lib/types.ts
@@ -20,6 +20,7 @@ export enum PropNames {
     AtlasName = 'AtlasName',
     Stage = 'Stage',
     Level = 'Level',
+    FileFormat = 'FileFormat'
 }
 
 export const PropMap = {
@@ -51,6 +52,10 @@ export const PropMap = {
         prop: 'level',
         displayName: 'Level',
     },
+    [PropNames.FileFormat]: {
+        prop: 'fileFormat',
+        displayName: 'File Format'
+    }
 };
 
 export interface IFilterProps {
