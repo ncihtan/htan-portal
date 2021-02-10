7fac9332fb8f297c435c0562d762d3f84c796302
Onur Sumer <s.onur.sumer@gmail.com>
2021-02-10 12:13:57 -0500

    enable typechecking in dev mode

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* See https://github.com/vercel/next.js/issues/12735#issuecomment-629404102 */
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+    /**
+     * Custom Webpack Config
+     * https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+     */
+    webpack(config, options) {
+        const { dev, isServer } = options;
+
+        // Do not run type checking twice:
+        if (dev && isServer) {
+            config.plugins.push(
+                new ForkTsCheckerWebpackPlugin()
+            );
+        }
+
+        return config;
+    },
+};
