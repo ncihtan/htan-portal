95ee84637a60251d524c49f3e13efd37f3c71187
Onur Sumer <s.onur.sumer@gmail.com>
2021-03-17 12:30:20 -0400

    improve filename truncate logic (fix #149)

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -424,23 +424,32 @@ export function getFileBase(filename: string) {
     return Path.basename(filename);
 }
 
-export function truncateFilename(filename: string, threshold: number = 15) {
+export function getFileExtension(filename: string) {
+    return Path.extname(filename);
+}
+
+export function getFilenameWithoutExtension(base: string) {
+    return base.includes('.') ? base.slice(0, base.lastIndexOf('.')) : base;
+}
+
+export function truncateFilename(
+    filename: string,
+    leadThreshold: number = 10,
+    trailThreshold: number = 5
+) {
     const base = getFileBase(filename);
-    const indexOfFirstDot = base.indexOf('.');
-    let name = base;
-    let ext = '';
+    const ext = getFileExtension(filename);
+    const name = getFilenameWithoutExtension(base);
 
-    if (indexOfFirstDot > 0) {
-        name = base.slice(0, indexOfFirstDot);
-        ext = base.slice(indexOfFirstDot + 1);
-    }
+    let displayValue = base;
 
-    let displayValue = name;
-
-    if (name.length > threshold) {
-        displayValue = `${name.slice(0, threshold)}...${ext}`;
-    } else if (ext.length > 0) {
-        displayValue = `${name}.${ext}`;
+    if (name.length > leadThreshold + trailThreshold) {
+        // get the first <leadThreshold> characters of the name
+        const lead = name.slice(0, leadThreshold);
+        // get the last <trailThreshold> characters of the name
+        const trail = name.slice(-trailThreshold);
+        // always keep the extension (everything after the last dot)
+        displayValue = `${lead}...${trail}${ext}`;
     }
 
     return displayValue;
