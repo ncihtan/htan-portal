aafc39b766f08835066e7555037fe1648aa53797
alisman <lisman.aaron@gmail.com>
2021-03-22 17:45:10 -0400

    fix footer to bottom of page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,8 @@ import Row from 'react-bootstrap/Row';
 
 const Footer = () => (
     <footer>
-        <Row className="justify-content-md-center mt-5 mb-5">
-            <small>
-                Human Tumor Atlas Network (HTAN) @ National Cancer Institute{' '}
-                {new Date().getFullYear()}
-            </small>
-        </Row>
+        Human Tumor Atlas Network (HTAN) @ National Cancer Institute{' '}
+        {new Date().getFullYear()}
     </footer>
 );
 
