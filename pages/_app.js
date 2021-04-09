8e3ebebef4bbdd38f7f18082bb3ab961d75fa3a2
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-04-09 18:03:06 -0400

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/app.scss';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import 'rc-tooltip/assets/bootstrap.css';
+import '../styles/app.scss';
+import * as gtag from '../lib/gtag';
 
 function App({ Component, pageProps }) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
     return <Component {...pageProps} />;
 }
 
