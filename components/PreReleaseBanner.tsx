ad18cfd31d6362cf9c7bdddc80e8a5d8698af31a
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-04-21 18:25:18 -0400

    indicate this is alpha release phase

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -2,7 +2,7 @@ import Alert from 'react-bootstrap/Alert';
 
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'warning'}>
-        The HTAN Data Portal is in a pre-release phase. Data cannot be
+        The HTAN Data Portal is in the alpha release phase. No data can be
         downloaded yet.{' '}
         <a href="https://github.com/ncihtan/htan-portal/issues" target="_blank">
             Feedback
