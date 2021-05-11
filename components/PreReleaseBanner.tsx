dcdb779baf4b17a7b9a5dbc4a37be98689e57702
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-05-11 10:27:24 -0400

    Fix #287
    Indicate we can download level 3&4 files now

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -2,8 +2,11 @@ import Alert from 'react-bootstrap/Alert';
 
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'warning'}>
-        The HTAN Data Portal is in the alpha release phase. No data can be
-        downloaded yet.{' '}
+        The HTAN Data Portal is in the alpha release phase. Only{' '}
+        <a href='/explore?tab=file&selectedFilters=%5B%7B"value"%3A"Level+3"%2C"label"%3A"Level+3"%2C"group"%3A"Level"%2C"count"%3A559%2C"isSelected"%3Afalse%7D%2C%7B"value"%3A"Level+4"%2C"label"%3A"Level+4"%2C"group"%3A"Level"%2C"count"%3A306%2C"isSelected"%3Afalse%7D%5D'>
+            level 3 and 4 data
+        </a>{' '}
+        can be downloaded.{' '}
         <a href="https://github.com/ncihtan/htan-portal/issues" target="_blank">
             Feedback
         </a>{' '}
