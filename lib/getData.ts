bd4e4df0bf3ffe931a007a899245143ef517ffa1
alisman <lisman.aaron@gmail.com>
2021-07-05 22:13:16 -0400

    stuff

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,6 +1,9 @@
-import data from '../data/syn_data.json';
+//import data from '../data/syn_data.json';
 import { SynapseData } from './types';
 
 export default function getData() {
-    return (data as unknown) as SynapseData;
+    return {
+        atlases: [],
+        schemas: [],
+    };
 }
