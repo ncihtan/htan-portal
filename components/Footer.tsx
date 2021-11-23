593ffa5d095f8e7f1da5857bb6686fe0384d518b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-11-23 13:53:00 -0500

    Add contact email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,9 @@ import Row from 'react-bootstrap/Row';
 
 const Footer = () => (
     <footer className={'pageFooter'}>
-        Human Tumor Atlas Network (HTAN) @ National Cancer Institute{' '}
-        {new Date().getFullYear()}
+        Human Tumor Atlas Network (HTAN) | National Cancer Institute{' '}
+        {new Date().getFullYear()} |{' '}
+        <a href="mailto:htan@googlegroups.com">htan@googlegroups.com</a>
     </footer>
 );
 
--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -6,11 +6,8 @@ const PreReleaseBanner = () => (
         <a href='/explore?tab=file&selectedFilters=%5B%7B"value"%3A"Level+3"%2C"label"%3A"Level+3"%2C"group"%3A"Level"%2C"count"%3A559%2C"isSelected"%3Afalse%7D%2C%7B"value"%3A"Level+4"%2C"label"%3A"Level+4"%2C"group"%3A"Level"%2C"count"%3A306%2C"isSelected"%3Afalse%7D%5D'>
             level 3 and 4 data
         </a>{' '}
-        can be downloaded.{' '}
-        <a href="https://github.com/ncihtan/htan-portal/issues" target="_blank">
-            Feedback
-        </a>{' '}
-        is welcome!
+        can be downloaded. Please send any feedback to{' '}
+        <a href="mailto:htan@googlegroups.com">htan@googlegroups.com</a>
     </Alert>
 );
 
