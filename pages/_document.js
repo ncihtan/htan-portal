38c88767a7a0b1e3d5604b75bea04c7cf571f8e2
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2022-04-20 16:52:34 -0400

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,7 @@ export default class MyDocument extends Document {
           `,
                         }}
                     />
+                    <link rel="shortcut icon" href="/favicon.ico"></link>
                 </Head>
                 <body>
                     <Main />
