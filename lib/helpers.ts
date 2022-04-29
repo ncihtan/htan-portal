665f9e74d6aaa3522381527e05b5b1cc1020fc4e
alisman <lisman.aaron@gmail.com>
2022-04-29 13:55:47 -0400

    Use gzipped data file on s3

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -128,7 +128,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : 'https://d13ch66cwesneh.cloudfront.net/processed_syn_data.json';
+            : 'https://d13ch66cwesneh.cloudfront.net/data.json';
     const res = await fetch(processedSynURL);
 
     // const json = await res.json();
