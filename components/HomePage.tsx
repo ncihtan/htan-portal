f8d78a27f60c19e610e4d3dba4f3177dacd85b58
Ino de Bruijn <ino@ino.bio>
2022-05-18 17:30:24 -0400

    Update homepage last update date

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -76,7 +76,7 @@ const HomePage: React.FunctionComponent<IHomePropsProps> = ({
                         color: '#fff',
                     }}
                 >
-                    <a style={{color:'white'}} href="/data-updates">Data Release V2 (Last updated 2022-03-30)</a>
+                    <a style={{color:'white'}} href="/data-updates">Data Release V2 (Last updated 2022-05-18)</a>
                 </div>
                 <Row className="justify-content-md-center">
                     <Col md={{ span: 5 }} style={{ color: '#fff', backgroundColor: 'rgba(0,0,0,0.5)', borderRadius: 25, padding: 20 }}>
