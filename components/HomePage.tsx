e71df24e5d3ef2f44fd0bb8fa83eb657df752da6
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2022-06-10 11:34:20 -0400

    Fix typo in news date

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -77,7 +77,7 @@ const HomePage: React.FunctionComponent<IHomePropsProps> = ({
                     }}
                 >
                     <a style={{ color: 'white' }} href="/data-updates">
-                        Data Release V2 (Last updated 2022-06-06)
+                        Data Release V2 (Last updated 2022-06-10)
                     </a>
                 </div>
                 <Row className="justify-content-md-center">
