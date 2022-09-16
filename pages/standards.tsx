55d5e731119788e1e40fd1e3995b5023f637d677
Ethan Cerami <cerami@gmail.com>
2022-09-16 14:53:37 -0400

    Rewrote intro page

--- pages/standards.tsx
+++ pages/standards.tsx
@@ -22,58 +22,97 @@ const Standards = (data: StandardsProps) => {
             <PageWrapper>
                 <Container>
                     <Row>
-                        <h1>Data Standards</h1>
+                        <div className="standards-content">
+                            <h1>HTAN Data Standards</h1>
 
-                        <p>
-                            The HTAN Network has adopted a set of data standards
-                            covering clinical, biospecimen, and assay metadata.
-                            As standards are finalized, we will publish their
-                            specifications below.
-                        </p>
-
-                        <h2>Browse Standards</h2>
-
-                        <ul>
-                            <li>
-                                <Link href="/standard/design">
-                                    Design Principles
-                                </Link>
-                            </li>
-                            <li>
+                            <p>
+                                All HTAN Centers are required to encode their
+                                data and metadata in the common HTAN Data Model.
+                                The HTAN Data Model was created via a community
+                                Request for Comment (RFC) process, with
+                                participation from all HTAN Centers, and covers{' '}
                                 <Link href="/standard/clinical">
-                                    Clinical Data
+                                    clinical data
                                 </Link>
-                            </li>
-                            <li>
+                                ,{' '}
                                 <Link href="/standard/biospecimen">
-                                    Biospecimen
+                                    biospecimen data
                                 </Link>
-                            </li>
-                            <li>
-                                <Link href="/standard/imaging">Imaging</Link>
-                            </li>
-                            <li>
-                                <Link href="/standard/rnaseq">
-                                    Single Cell and Single Nucleus RNA Seq
-                                    (sc/snRNASeq)
+                                ,{' '}
+                                <Link href="/standard/sequencing">
+                                    genomic sequencing data
+                                </Link>{' '}
+                                and{' '}
+                                <Link href="/standard/imaging">
+                                    multiplex imaging data
                                 </Link>
-                            </li>
-                            <li>
-                                <Link href="/standard/scatacseq">
-                                    Single Cell ATAC Seq
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/standard/bulkrnaseq">
-                                    Bulk RNA Seq
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/standard/bulkdnaseq">
-                                    Bulk DNA Seq
-                                </Link>
-                            </li>
-                        </ul>
+                                .
+                            </p>
+                            <p>
+                                As much as possible, the HTAN Data Model
+                                leverages previously defined data standards
+                                across the scientific research community,
+                                including the{' '}
+                                <a href="https://gdc.cancer.gov/">
+                                    NCI Genomic Data Commons
+                                </a>
+                                , the{' '}
+                                <a href="https://www.humancellatlas.org/">
+                                    Human Cell Atlas
+                                </a>
+                                , the{' '}
+                                <a href="https://hubmapconsortium.org/">
+                                    Human Biomolecular Atlas Program (HuBMAP)
+                                </a>{' '}
+                                and the{' '}
+                                <a href="https://www.miti-consortium.org/">
+                                    Minimum Information about Tissue Imaging
+                                    (MITI)
+                                </a>{' '}
+                                reporting guidelines.
+                            </p>
+
+                            <p>
+                                HTAN uses{' '}
+                                <a href="https://bioschemas.org/">bioschemas</a>{' '}
+                                to define the data model. Bioschema extends{' '}
+                                <a href="https://schema.org/">schema.org</a>, a
+                                community effort used by many search engines
+                                that provides a way to define information with
+                                properties. Bioschemas define profiles over
+                                types that state which properties must be used
+                                (minimum), should be used (recommended), and
+                                could be used (optional). HTAN and other
+                                consortiums, including the Human Cell Atlas and
+                                HuBMAP are working together to provide common
+                                shared schemas.
+                            </p>
+
+                            <h2>Browse Standards</h2>
+
+                            <ul>
+                                <li>
+                                    <Link href="/standard/clinical">
+                                        Clinical
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/standard/biospecimen">
+                                        Biospecimen
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/standard/sequencing">
+                                        Sequencing
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/standard/imaging">
+                                        Imaging
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </Row>
                 </Container>
             </PageWrapper>
