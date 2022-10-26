00570026d7a1349431f83be3a771bffb58e86aaf
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2022-10-26 11:35:33 -0400

    reintroduce vanderbilt cellxgene instance

--- components/WPAtlasTable.tsx
+++ components/WPAtlasTable.tsx
@@ -310,14 +310,14 @@ export default class WPAtlasTable extends React.Component<IWPAtlasTableProps> {
             {
                 name: 'Atlas Name',
                 selector: (atlas: Atlas) => atlas.htan_name,
-                grow: 1.25,
+                grow: 1.2,
                 wrap: true,
                 sortable: true,
             },
             {
                 name: 'Lead Institution',
                 selector: (atlas: Atlas) => atlas.WPAtlas.lead_institutions,
-                grow: 2.5,
+                grow: 1.6,
                 wrap: true,
                 sortable: true,
             },
@@ -506,29 +506,12 @@ export default class WPAtlasTable extends React.Component<IWPAtlasTableProps> {
                     } else if (atlas.htan_name === 'HTAN Vanderbilt') {
                         return (
                             <>
-                                {/* Outddated Cellxgene Instance */}
-                                {/*<CellxgeneViewerLink url={'https://cellxgene.cziscience.com/d/9899E3D8-ACE5-40BD-AC93-7AB7CE2AEC70.cxg/'} count={1} />*/}
-                                <Tooltip overlay="cellxgene temporarily disabled (update pending)">
-                                    <a
-                                        style={{
-                                            paddingRight: 8,
-                                            fontFamily: 'monospace',
-                                            textDecoration: 'none',
-                                            cursor: 'no-drop',
-                                        }}
-                                    >
-                                        {'\u00A0'}
-                                        {'\u00A0'}
-                                        {1}{' '}
-                                        <img
-                                            width={20}
-                                            style={{ filter: 'grayscale(1)' }}
-                                            src={
-                                                'https://pbs.twimg.com/profile_images/1285714433981812736/-wuBO62N_400x400.jpg'
-                                            }
-                                        />
-                                    </a>
-                                </Tooltip>
+                                <CellxgeneViewerLink
+                                    url={
+                                        'https://cellxgene.cziscience.com/collections/a48f5033-3438-4550-8574-cdff3263fdfd'
+                                    }
+                                    count={3}
+                                />
                                 <BigQueryLink
                                     url={
                                         '/explore?selectedFilters=%5B%7B"value"%3A"hdf5"%2C"label"%3A"hdf5"%2C"group"%3A"FileFormat"%2C"count"%3A11%2C"isSelected"%3Afalse%7D%2C%7B"value"%3A"HTAN+Vanderbilt"%2C"label"%3A"HTAN+Vanderbilt"%2C"group"%3A"AtlasName"%2C"count"%3A4%2C"isSelected"%3Afalse%7D%5D&tab=file'
