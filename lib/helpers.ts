5e162e949dcf7e7100c2a835bf0cea7d82bed14e
Onur Sumer <s.onur.sumer@gmail.com>
2022-12-08 14:21:57 -0500

    update preview data

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -127,7 +127,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : 'https://d13ch66cwesneh.cloudfront.net/processed_syn_data_20221207.json';
+            : 'https://d13ch66cwesneh.cloudfront.net/processed_syn_data_20221208.json';
     const res = await fetch(processedSynURL);
 
     // const json = await res.json();
