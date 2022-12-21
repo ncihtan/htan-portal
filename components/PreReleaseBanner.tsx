0a3292bd9052f5116e93ddd5719f03a75db39c00
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2022-12-21 10:19:57 -0500

    Advertise v3 in banner

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -2,8 +2,9 @@ import Alert from 'react-bootstrap/Alert';
 
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'warning'}>
-        The HTAN Portal is in the beta release phase. Most of the data shown are{' '}
-        <a href="/data-download">downloadable</a>. Please send any questions or
+        The <a href='/data-updates'>V3 data release</a> is now
+        finalized. Most of the data shown are{' '} <a
+        href="/data-download">downloadable</a>. Please send any questions or
         feedback to{' '}
         <a href="mailto:htan@googlegroups.com">htan@googlegroups.com</a>
     </Alert>
