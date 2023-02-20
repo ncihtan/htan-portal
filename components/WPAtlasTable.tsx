2916cfe8ebb9eec66bad792a402f1147d593ee72
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-02-20 12:05:57 -0500

    fix missing metadata

--- components/WPAtlasTable.tsx
+++ components/WPAtlasTable.tsx
@@ -61,7 +61,7 @@ const SynapseDataLink = (props: { id: string }) => (
 
 const MetaDataLink = (props: { id: string }) => (
     <a
-        href={`https://https://htan-metadata-20221219-1119am.surge.sh/${props.id}.csv`}
+        href={`https://htan-metadata-20220220-1202.surge.sh/${props.id}.csv`}
         download
     >
         {props.id}
