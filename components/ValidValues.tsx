b04d270c07f87bfa950ac7e1a794ada237246b31
Onur Sumer <s.onur.sumer@gmail.com>
2023-04-07 14:55:07 -0400

    add modal to show all valid values

--- components/ValidValues.tsx
+++ components/ValidValues.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TruncateMarkup, { TruncateProps } from 'react-truncate-markup';
+import styles from './common.module.scss';
+import { Modal } from 'react-bootstrap';
 
 interface ValidValuesProps {
+    attribute: string;
     attributes: string[];
     truncateProps?: TruncateProps;
 }
 
+interface ViewAllValuesModalProps {
+    attribute: string;
+    options: JSX.Element[];
+    show: boolean;
+    onClose: () => void;
+}
+
+const ViewAllValuesModal: React.FunctionComponent<ViewAllValuesModalProps> = (
+    props
+) => {
+    return (
+        <Modal show={props.show} onHide={props.onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{props.attribute} valid values</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{props.options}</Modal.Body>
+        </Modal>
+    );
+};
+
 const ValidValues: React.FunctionComponent<ValidValuesProps> = (props) => {
+    const [showModal, setShowModal] = useState(false);
+    const onModalClose = () => setShowModal(false);
+    const onClick = () => setShowModal(true);
+
     const options = props.attributes
         .filter((attribute) => attribute.length > 0)
         .map((attribute) => <div>- {attribute.toLowerCase()}</div>);
 
     const ellipsis = (
         <div>
-            ... <i>Number of valid options: {props.attributes.length}.</i>
+            <ViewAllValuesModal
+                attribute={props.attribute}
+                options={options}
+                show={showModal}
+                onClose={onModalClose}
+            />
+            ... <i>Number of valid options: {options.length}</i> (
+            <span className={styles.clickable} onClick={onClick}>
+                Show all
+            </span>
+            )
         </div>
     );
 
