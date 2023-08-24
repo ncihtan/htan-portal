a1913a19ea95442c1f257e962434bbf0e7f1515b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-08-24 14:53:27 -0400

    fix missing findAndReplace step

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -147,7 +147,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : 'https://d13ch66cwesneh.cloudfront.net/processed_syn_data_20230824_1433.json';
+            : 'https://d13ch66cwesneh.cloudfront.net/processed_syn_data_20230824_1451.json';
     const res = await fetch(processedSynURL);
 
     // const json = await res.json();
