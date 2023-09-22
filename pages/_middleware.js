86f0b39ffef05f27ad0198704ef2f8913e025dc1
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-09-22 19:12:14 -0400

    add comment about middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,10 +1,13 @@
+// A hack to deal with this silliness https://github.com/vercel/next.js/issues/21498
 import { NextResponse } from 'next/server';
 
 const Middleware = (req) => {
-  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
-    return NextResponse.next();
+    if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
+        return NextResponse.next();
 
-  return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
+    return NextResponse.redirect(
+        new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
+    );
 };
 
 export default Middleware;
