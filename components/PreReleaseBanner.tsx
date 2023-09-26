d9b8d19d59d10171de326eb374e3d1b154b24e31
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-09-26 15:15:48 -0400

    add schedule to jamboree announcement

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -3,7 +3,7 @@ import Alert from 'react-bootstrap/Alert';
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'success'}>
         Join the virtual{' '}
-        <a href="/jamboree">HTAN Data Jamboree December 4th - 8th</a>!!
+        <a href="/jamboree">HTAN Data Jamboree December 4th - 7th</a>!!
         {/*The <a href="/data-updates">V4 data release</a> is now finalized. Most
         of the data shown are <a href="/data-access">available</a>. Please send
         any questions or feedback to{' '}
