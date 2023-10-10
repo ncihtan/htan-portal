caf79607e7aa76a0886880ea57283144943c8493
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-10-10 09:30:49 -0400

    make url lower to uppercase conversion more stringent

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,8 +3,12 @@ import { NextResponse } from 'next/server';
 const Middleware = (req) => {
     // A hack to deal with this case sensitive silliness
     // https://github.com/vercel/next.js/issues/21498
-    // Restrict to HTA because otherwise it breaks some images/fonts
-    if (req.nextUrl.pathname.startsWith('/HTA')) {
+    // Restrict to HTA pages because otherwise it breaks some images/fonts
+    if (
+        req.nextUrl.pathname.startsWith('/HTA') &&
+        !req.nextUrl.pathname.endsWith('.jpeg') &&
+        !req.nextUrl.pathname.endsWith('.png')
+    ) {
         return NextResponse.redirect(
             new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
         );
