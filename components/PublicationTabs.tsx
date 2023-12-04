888cfd62de583bf2ab4a261bf2f43207364780bc
Xiang Li <xiangli5699@gmail.com>
2023-12-04 18:43:29 -0500

    Add publication page for HTAPP MBC

--- components/PublicationTabs.tsx
+++ components/PublicationTabs.tsx
@@ -237,6 +237,7 @@ const toolsContent: { [id: string]: JSX.Element[] } = {
             </Tooltip>
         </>,
     ],
+    htapp_mbc_klughammer_2023: [<></>],
 };
 
 const supportingLinks: { [id: string]: JSX.Element[] } = {
