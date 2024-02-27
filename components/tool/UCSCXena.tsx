605a0d37b446c1a98ce9d6d0ed3ca9ea988a065b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2024-02-26 21:35:24 -0600

    add UCSC tool description

---
+++ components/tool/UCSCXena.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const UCSCXena: React.FunctionComponent = () => {
+    return (
+        <div>
+            <h3>UCSCXena</h3>
+            <p>
+                <a href="https://xena.ucsc.edu/">UCSCXena</a> is an
+                interactive viewer for single-cell expression.
+            </p>
+            <p>
+                UCSC Xena is an online exploration tool for multiplexed imaging and multi-omic single-cell data. 
+                <a href="https://xena.ucsc.edu/">
+                    <img src="https://github.com/cBioPortal/cbioportal/assets/1334004/0f58a05e-5c4d-42a2-8d0d-61b630dc2e25"
+                    />
+                </a>
+                HTAN melanoma sample t-CyCIF image showing three antibody signals. Genomic data is overlaid as circles, coloring cells annotated as keratinocytes in purple. Users can also view an interactive 3D UMAP/tSNE of the same data. To navigate the tool, select spatial or embedding layouts, manipulate image layers, and choose the genomics data to color the cells.
+            </p>
+        </div>
+    );
+}
--- components/tool/toolDetails.tsx
+++ components/tool/toolDetails.tsx
@@ -4,6 +4,7 @@ import Mcmicro from './Mcmicro';
 import GoogleBigQuery from './GoogleBigQuery';
 import CellxGene from './CellxGene';
 import CytoCommunity from './CytoCommunity';
+import UCSCXena from './CytoCommunity';
 
 export const ToolDetails: { [toolID: string]: JSX.Element } = {
     HTA8_T0: <CBioPortal />,
@@ -11,5 +12,6 @@ export const ToolDetails: { [toolID: string]: JSX.Element } = {
     HTA7_T2: <Mcmicro />,
     HTAX_T0: <GoogleBigQuery />,
     HTAX_T1: <CellxGene />,
+    HTAX_T2: <UCSCXena />,
     HTA4_T0: <CytoCommunity />,
 };
