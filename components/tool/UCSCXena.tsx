3ba332bfb6fd568d3148d74a4d4118d46368239b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2024-02-26 21:56:49 -0600

    tweak style xena modal

--- components/tool/UCSCXena.tsx
+++ components/tool/UCSCXena.tsx
@@ -11,9 +11,11 @@ const UCSCXena: React.FunctionComponent = () => {
             <p>
                 UCSC Xena is an online exploration tool for multiplexed imaging and multi-omic single-cell data. 
                 <a href="https://xena.ucsc.edu/">
-                    <img src="https://github.com/cBioPortal/cbioportal/assets/1334004/0f58a05e-5c4d-42a2-8d0d-61b630dc2e25"
+                    <img
+                        width="600"
+                        src="https://github.com/cBioPortal/cbioportal/assets/1334004/0f58a05e-5c4d-42a2-8d0d-61b630dc2e25"
                     />
-                </a>
+                </a><br />
                 HTAN melanoma sample t-CyCIF image showing three antibody signals. Genomic data is overlaid as circles, coloring cells annotated as keratinocytes in purple. Users can also view an interactive 3D UMAP/tSNE of the same data. To navigate the tool, select spatial or embedding layouts, manipulate image layers, and choose the genomics data to color the cells.
             </p>
         </div>
