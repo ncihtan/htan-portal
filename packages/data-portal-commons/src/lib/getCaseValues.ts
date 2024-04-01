550f12528415556a79e29e96f5fcbef61af24e8a
Tatiana <tatiana.matiukhova@gmail.com>
2024-04-01 09:51:08 -0700

    add more coarse filter for organ type on the explore page

--- packages/data-portal-commons/src/lib/getCaseValues.ts
+++ packages/data-portal-commons/src/lib/getCaseValues.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { Entity } from './entity';
+import { getNormalizedOrgan } from '@htan/data-portal-commons';
 
 export function getCaseValues(propName: keyof Entity) {
     return (e: Entity) => {
@@ -7,6 +8,16 @@ export function getCaseValues(propName: keyof Entity) {
             return _.uniq(e.cases.map((c) => c[propName] as string));
         } else {
             return [e[propName] as string];
+        }
+    };
+}
+
+export function getNormalizedOrganCaseValues() {
+    return (e: Entity) => {
+        if (e.cases) {
+            return _.uniq(e.cases.map((c) => getNormalizedOrgan(c) as string));
+        } else {
+            return [getNormalizedOrgan(e) as string];
         }
     };
 }
