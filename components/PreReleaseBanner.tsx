a138b98feda04bc564a432a3c31c6df89269d833
Ino de Bruijn <ino@ino.bio>
2024-04-22 12:12:41 -0400

    slight change to text

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -2,16 +2,9 @@ import Alert from 'react-bootstrap/Alert';
 import Link from 'next/link';
 
 const PreReleaseBanner = () => (
-    <Alert style={{ marginBottom: 0 }} variant={'info'}>
+    <Alert style={{ marginBottom: 0 }} variant={'warning'}>
         Open access (Level 3 and 4) files are currently unavailable through Synapse.
-        Please visit the{' '}
-        <a
-            href="https://sagebionetworks.jira.com/servicedesk/customer/portal/1"
-            target="_blank"
-        >
-            HTAN Help Desk
-        </a>{' '}
-        for questions and feedback.
+        The HTAN DCC will be resetting Synapse access controls on HTAN projects on Monday 22 April.
     </Alert>
 );
 
