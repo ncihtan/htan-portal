81f9974dedf68a35f1fcb93510a249b9f60f85d6
Ino de Bruijn <ino@ino.bio>
2024-05-10 10:03:06 -0400

    fix typo

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'success'}>
-        New data from BU, HTAPP, CHOP, OHSU, and Standford is available as part
+        New data from BU, HTAPP, CHOP, OHSU, and Stanford is available as part
         of <Link href="/data-updates">Release 5.1</Link>!
     </Alert>
 );
