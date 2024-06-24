1478e5f1241e7447eaa769db0eb5d48e65fcd2f8
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-06-24 13:54:22 +0530

    ♻️ Refactor code: simplified search logic

--- lib/dataStandardFilterUtils.ts
+++ lib/dataStandardFilterUtils.ts
@@ -1,119 +1,63 @@
-import { action, makeObservable, observable } from 'mobx';
-import {
-    FilterActionMeta,
-    SelectedFilter,
-    getNewFilters,
-} from '@htan/data-portal-filter';
-import {
-    DataSchemaData,
-    getDataSchemaDependencies,
-} from '@htan/data-portal-schema';
+import { action, makeObservable, observable, computed } from 'mobx';
+import { DataSchemaData } from '@htan/data-portal-schema';
 
 export class DataStandardFilterStore {
-    @observable allAttributeNames: string[] = [];
-    @observable selectedAttributes: string[] = [];
-    @observable private _selectedFilters: SelectedFilter[] = [];
+    @observable searchText: string = '';
+    private schemaData: DataSchemaData[];
+    private dataSchemaMap: { [id: string]: DataSchemaData };
 
     constructor(
         schemaData: DataSchemaData[],
-        schemaDataById: Record<string, DataSchemaData> = {}
+        dataSchemaMap: { [id: string]: DataSchemaData }
     ) {
         makeObservable(this);
-        this.getAttributeNames(schemaData, schemaDataById);
+        this.schemaData = schemaData;
+        this.dataSchemaMap = dataSchemaMap;
     }
 
-    @action
-    getAttributeNames = (
-        schemaData: DataSchemaData[],
-        schemaDataById: Record<string, DataSchemaData>,
-        attributeNames = new Set<string>()
-    ) => {
-        if (!schemaData) return attributeNames;
-
-        schemaData.forEach((data) => {
-            attributeNames.add(data.attribute);
-
-            const dependencies = getDataSchemaDependencies(
-                data,
-                schemaDataById
-            );
-            this.getAttributeNames(
-                dependencies,
-                schemaDataById,
-                attributeNames
-            );
-        });
-
-        this.allAttributeNames = Array.from(attributeNames);
-    };
-
     @action.bound
-    handleSearch = (searchQuery: string) => {
-        const searchedAttributes = this.allAttributeNames.filter((attribute) =>
-            attribute.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-        this.selectedAttributes = searchedAttributes;
-    };
+    setSearchText(text: string) {
+        this.searchText = text;
+    }
 
-    @action.bound
-    handleFilterChange = (
-        filters: SelectedFilter[] | FilterActionMeta<SelectedFilter>
-    ) => {
-        if (!Array.isArray(filters)) {
-            if (filters.action === 'select-option' && filters.option) {
-                this._selectedFilters.push(filters.option);
-            } else {
-                return;
-            }
-        } else {
-            this._selectedFilters = filters;
+    @computed
+    get filteredSchemaDataById(): { [id: string]: DataSchemaData } {
+        const searchTextLower = this.searchText.toLowerCase();
+        const schemaDataIds = new Set(this.schemaData.map((data) => data.id));
+
+        if (searchTextLower.length === 0) {
+            // Default logic when nothing is searched for
+            const entries = Object.entries(
+                this.dataSchemaMap
+            ).filter(([key, value]) =>
+                this.schemaData.some((data) => data.id === value.id)
+            );
+            return Object.fromEntries(entries);
         }
 
-        this.updateSelectedAttributes();
-    };
+        const filteredEntries = Object.entries(this.dataSchemaMap).filter(
+            ([key, value]) => {
+                const attributeMatches = value.attribute
+                    .toLowerCase()
+                    .includes(searchTextLower);
+                const descriptionMatches = value.description
+                    .toLowerCase()
+                    .includes(searchTextLower);
+                const parentIdMatches = value.parentIds.some((parentId) =>
+                    schemaDataIds.has(parentId)
+                );
 
-    @action.bound
-    clearFilter = (filterValue: string) => {
-        this._selectedFilters = this._selectedFilters.filter(
-            (filter) => filter.value !== filterValue
+                return (
+                    (attributeMatches || descriptionMatches) && parentIdMatches
+                );
+            }
         );
-        this.updateSelectedAttributes();
-    };
 
-    @action.bound
-    clearAllFilters = () => {
-        this._selectedFilters = [];
-        this.selectedAttributes = this.allAttributeNames;
-    };
-
-    @action.bound
-    updateSelectedAttributes = () => {
-        const selectedAttributeValues = this._selectedFilters.map(
-            (filter) => filter.value
-        );
-        this.selectedAttributes = this.allAttributeNames.filter((attribute) =>
-            selectedAttributeValues.includes(attribute)
-        );
-    };
-
-    @action.bound
-    updateSelectedAttributesDirectly = (attributes: string[]) => {
-        this.selectedAttributes = attributes;
-    };
-
-    getSelectedFilters = () => this._selectedFilters;
-
-    get selectedFilters(): SelectedFilter[] {
-        return this._selectedFilters;
+        return Object.fromEntries(filteredEntries);
     }
 
-    set selectedFilters(filters: SelectedFilter[]) {
-        this._selectedFilters = filters;
-        this.updateSelectedAttributes();
-    }
-
-    @action.bound
-    setFilter(actionMeta: FilterActionMeta<SelectedFilter>) {
-        this.selectedFilters = getNewFilters(this.selectedFilters, actionMeta);
+    @computed
+    get isFiltered(): boolean {
+        return this.searchText.length > 0;
     }
 }
