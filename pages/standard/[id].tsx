537ef4090c15baae6559cc7831933c1c63f449ae
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-07-22 10:24:41 +0530

    ✨New Feature: All Attributes Tab added, fixing back page, biospecimen manifest page display fixed

--- pages/standard/[id].tsx
+++ pages/standard/[id].tsx
@@ -1,3 +1,83 @@
+// import React from 'react';
+// import { GetServerSideProps } from 'next';
+// import {
+//     getDataSchema,
+//     SchemaDataId,
+//     DataSchemaData,
+// } from '@htan/data-portal-schema';
+// import { HtanNavbar } from '../../components/HtanNavbar';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Link from 'next/link';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+// import DataSchema from '../../components/DataSchema';
+// import Footer from '../../components/Footer';
+
+// interface ManifestProps {
+//     schemaData: DataSchemaData;
+//     requiredDependencies: DataSchemaData[];
+//     schemaDataById: { [id: string]: DataSchemaData };
+// }
+
+// const Manifest: React.FC<ManifestProps> = ({
+//     schemaData,
+//     requiredDependencies,
+//     schemaDataById,
+// }) => {
+//     return (
+//         <>
+//             <HtanNavbar />
+//             <Container>
+//                 <Row style={{ marginBottom: 10 }}>
+//                     <Col>
+//                         <Link href="/standards">
+//                             <a>
+//                                 <FontAwesomeIcon icon={faArrowLeft} />
+//                                 &nbsp; Back to Data Standards
+//                             </a>
+//                         </Link>
+//                     </Col>
+//                 </Row>
+//                 <Row>
+//                     <Col>
+//                         <h1>{schemaData.attribute} Manifest</h1>
+//                     </Col>
+//                 </Row>
+//                 <Row>
+//                     <Col>
+//                         <DataSchema
+//                             schemaData={requiredDependencies}
+//                             dataSchemaMap={schemaDataById}
+//                         />
+//                     </Col>
+//                 </Row>
+//             </Container>
+//             <Footer />
+//         </>
+//     );
+// };
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//     const { id } = context.params as { id: string };
+//     const fullId = `bts:${id}` as SchemaDataId;
+//     const { schemaDataById } = await getDataSchema([fullId]);
+//     const schemaData = schemaDataById[fullId];
+
+//     const requiredDependencies = (schemaData.requiredDependencies || []).map(
+//         (depId: string | { '@id': string }) => {
+//             const depSchemaId =
+//                 typeof depId === 'string' ? depId : depId['@id'];
+//             return schemaDataById[depSchemaId];
+//         }
+//     );
+
+//     return { props: { schemaData, requiredDependencies, schemaDataById } };
+// };
+
+// export default Manifest;
+
 import React from 'react';
 import { GetServerSideProps } from 'next';
 import {
@@ -19,12 +99,14 @@ interface ManifestProps {
     schemaData: DataSchemaData;
     requiredDependencies: DataSchemaData[];
     schemaDataById: { [id: string]: DataSchemaData };
+    referer: string;
 }
 
 const Manifest: React.FC<ManifestProps> = ({
     schemaData,
     requiredDependencies,
     schemaDataById,
+    referer,
 }) => {
     return (
         <>
@@ -32,10 +114,10 @@ const Manifest: React.FC<ManifestProps> = ({
             <Container>
                 <Row style={{ marginBottom: 10 }}>
                     <Col>
-                        <Link href="/standards">
+                        <Link href={referer || '/standards'}>
                             <a>
                                 <FontAwesomeIcon icon={faArrowLeft} />
-                                &nbsp; Back to Data Standards
+                                &nbsp; Back
                             </a>
                         </Link>
                     </Col>
@@ -60,7 +142,7 @@ const Manifest: React.FC<ManifestProps> = ({
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { id } = context.params as { id: string };
+    const { id, referer } = context.query as { id: string; referer?: string };
     const fullId = `bts:${id}` as SchemaDataId;
     const { schemaDataById } = await getDataSchema([fullId]);
     const schemaData = schemaDataById[fullId];
@@ -73,7 +155,14 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         }
     );
 
-    return { props: { schemaData, requiredDependencies, schemaDataById } };
+    return {
+        props: {
+            schemaData,
+            requiredDependencies,
+            schemaDataById,
+            referer: referer || null,
+        },
+    };
 };
 
 export default Manifest;
