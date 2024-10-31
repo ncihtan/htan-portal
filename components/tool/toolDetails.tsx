c7bca0249fd298074a20bbea29d9c9a44fb519a4
Onur Sumer <s.onur.sumer@gmail.com>
2024-10-31 15:46:05 -0400

    add CelloType tool

--- components/tool/toolDetails.tsx
+++ components/tool/toolDetails.tsx
@@ -2,6 +2,7 @@ import CBioPortal from './cBioPortal';
 import Minerva from './Minerva';
 import Mcmicro from './Mcmicro';
 import GoogleBigQuery from './GoogleBigQuery';
+import CelloType from './CelloType';
 import CellxGene from './CellxGene';
 import CytoCommunity from './CytoCommunity';
 import UCSCXena from './UCSCXena';
@@ -14,4 +15,5 @@ export const ToolDetails: { [toolID: string]: JSX.Element } = {
     HTAX_T1: <CellxGene />,
     HTAX_T2: <UCSCXena />,
     HTA4_T0: <CytoCommunity />,
+    HTA4_T1: <CelloType />,
 };
