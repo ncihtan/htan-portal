008413f017133a88c40aaa84b60b837de4768bb9
Ino de Bruijn <ino@ino.bio>
2024-11-01 10:58:50 -0400

    update banner link

--- components/PreReleaseBanner.tsx
+++ components/PreReleaseBanner.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const PreReleaseBanner = () => (
     <Alert style={{ marginBottom: 0 }} variant={'success'}>
-        <a href="https://www.nature.com/collections/fihchcjehc" target="_blank">
+        <a href="https://www.nature.com/immersive/d42859-024-00059-y/index.html" target="_blank">
             Read the new collection of HTAN publications
         </a>
         !
