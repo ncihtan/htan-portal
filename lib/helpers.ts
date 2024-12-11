6f79b12de88609776175f90776c18f15d4c8e4f0
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2024-12-11 17:20:18 -0500

    additional fixes CDS mapping

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -44,7 +44,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : `${getCloudBaseUrl()}/processed_syn_data_20241206_1726.json`;
+            : `${getCloudBaseUrl()}/processed_syn_data_20241211_1718.json`;
     return fetchSynData(processedSynURL);
 }
 
