858b1ee153eb3cf81b2ecd9bc19196187eee932d
Onur Sumer <s.onur.sumer@gmail.com>
2025-01-16 14:28:12 -0500

    move more logic into processSynapseJSON

--- packages/data-portal-commons/src/lib/fillInEntities.ts
+++ packages/data-portal-commons/src/lib/fillInEntities.ts
@@ -8,16 +8,6 @@ export function fillInEntities(data: LoadDataResult): Entity[] {
     const demoMap = data.demographicsByParticipantID;
     const therapyMap = data.therapyByParticipantID;
 
-    // give each biospecimen it's caseid (i.e "diagnosis" HTANParticipantID)
-    // biospecimen have HTANParentID but that may or may not be it's caseid because
-    // biospecimen can have other biospecimen as parents (one case at top)
-    _.forEach(data.biospecimenByBiospecimenID, (specimen) => {
-        const parentIdMatch = specimen.ParentID.match(/[^_]*_[^_]*/);
-        // we should always have a match
-        specimen.ParticipantID =
-            specimen.ParticipantID || (parentIdMatch ? parentIdMatch[0] : '');
-    });
-
     // TODO we cannot do this, we need to deal with this in a different way in Plots tab
     // data.files = data.files.filter((f) => {
     //     return f.demographicsIds.length > 0 && f.diagnosisIds.length > 0;
@@ -38,13 +28,7 @@ export function fillInEntities(data: LoadDataResult): Entity[] {
         );
         (file as Entity).cases = _.uniqBy(
             mergeCaseData(
-                _.uniq(
-                    [
-                        ...(file as Entity).diagnosis,
-                        ...(file as Entity).demographics,
-                        ...(file as Entity).therapy,
-                    ].map((e) => e.ParticipantID)
-                ),
+                file.caseIds,
                 diagnosisMap as { [id: string]: Entity },
                 demoMap as { [id: string]: Entity },
                 therapyMap as { [id: string]: Entity }
