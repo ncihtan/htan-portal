b9dc70ea99cac473ae33112f3c59611990e0f672
Onur Sumer <s.onur.sumer@gmail.com>
2025-03-27 16:34:21 -0400

    update data

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -45,7 +45,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : `${getCloudBaseUrl()}/processed_syn_data_20250319_1350.json`;
+            : `${getCloudBaseUrl()}/processed_syn_data_20250327_1627.json`;
     return fetchSynData(processedSynURL);
 }
 
