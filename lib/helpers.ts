ec40b7faef38cd0f63123e7b15fa84dfe994cfd2
Onur Sumer <s.onur.sumer@gmail.com>
2025-04-17 15:40:44 -0400

    Update publication data (v32) (#796)
    * update publications manifest
    
    * update data

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -45,7 +45,7 @@ export async function fetchData(): Promise<LoadDataResult> {
     const processedSynURL =
         process.env.NODE_ENV === 'development'
             ? '/processed_syn_data.json'
-            : `${getCloudBaseUrl()}/processed_syn_data_20250416_1714.json`;
+            : `${getCloudBaseUrl()}/processed_syn_data_20250417_1154.json`;
     return fetchSynData(processedSynURL);
 }
 
