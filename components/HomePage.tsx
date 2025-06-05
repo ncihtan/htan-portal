7ea6368269ebf37a76da00371ad86ae0f8933657
Onur Sumer <s.onur.sumer@gmail.com>
2025-06-05 11:25:17 -0400

    update release notes

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -54,7 +54,7 @@ const HomePage: React.FunctionComponent<IHomePropsProps> = ({
                     }}
                 >
                     <a style={{ color: 'white' }} href="/data-updates">
-                        Data Release V6.2 (Last updated 2025-05-15)
+                        Data Release V6.3 (Last updated 2025-06-09)
                     </a>
                 </div>
                 <Row className="justify-content-md-center">
