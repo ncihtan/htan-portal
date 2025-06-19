cdf2ae1318956f1499bcb480dfdfec70e36e8334
Onur Sumer <s.onur.sumer@gmail.com>
2025-06-19 14:36:45 -0400

    fix publication table atlas column search

--- packages/data-portal-commons/src/components/AtlasDescription.tsx
+++ packages/data-portal-commons/src/components/AtlasDescription.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { AtlasMeta } from '../lib/entity';
 
+export function shouldIncludeAtlasHref(atlasMeta: AtlasMeta) {
+    return !['hta13', 'hta14', 'hta15'].includes(
+        atlasMeta.htan_id?.toLowerCase()
+    );
+}
+
+export function getAtlasDescription(atlasMeta: AtlasMeta, atlasName?: string) {
+    return shouldIncludeAtlasHref(atlasMeta)
+        ? atlasName || atlasMeta?.title?.rendered
+        : atlasMeta?.short_description;
+}
+
 export const AtlasDescription: React.FunctionComponent<{
     atlasMeta: AtlasMeta;
     atlasName?: string;
 }> = (props) => {
-    return !['hta13', 'hta14', 'hta15'].includes(
-        props.atlasMeta.htan_id?.toLowerCase()
-    ) ? (
+    const atlasDescription = getAtlasDescription(
+        props.atlasMeta,
+        props.atlasName
+    );
+    return shouldIncludeAtlasHref(props.atlasMeta) ? (
         <span>
             <a href={`/center/${props.atlasMeta.htan_id.toLowerCase()}`}>
-                {props.atlasName || props.atlasMeta?.title?.rendered}
+                {atlasDescription}
             </a>
         </span>
     ) : (
-        <span>{props.atlasMeta?.short_description}</span>
+        <span>{atlasDescription}</span>
     );
 };
 
