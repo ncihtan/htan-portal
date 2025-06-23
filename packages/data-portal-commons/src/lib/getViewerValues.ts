25c0a405126d72f24743e3e3522a0d8fc9bae712
Onur Sumer <s.onur.sumer@gmail.com>
2025-06-23 18:19:17 -0400

    fix incorrect viewer counts

--- packages/data-portal-commons/src/lib/getViewerValues.ts
+++ packages/data-portal-commons/src/lib/getViewerValues.ts
@@ -5,9 +5,13 @@ export function getViewerValues(
     entity: Entity,
     exclude: FileViewerName[] = [FileViewerName.cds, FileViewerName.idc]
 ): FileViewerName[] {
-    const viewers =
-        typeof entity.viewers === 'string'
-            ? JSON.parse(entity.viewers)
-            : entity.viewers;
-    return _(viewers).keys().difference(exclude).value() as FileViewerName[];
+    const viewers = _.isString(entity.viewers)
+        ? JSON.parse(entity.viewers)
+        : entity.viewers;
+
+    return _(viewers)
+        .omitBy(_.isUndefined)
+        .keys()
+        .difference(exclude)
+        .value() as FileViewerName[];
 }
