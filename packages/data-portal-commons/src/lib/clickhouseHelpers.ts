d54ba1713af82b9af98d8ce48756934c05735727
Onur Sumer <s.onur.sumer@gmail.com>
2025-09-09 10:33:50 -0400

    allow creating a new DB when importing data

--- packages/data-portal-commons/src/lib/clickhouseHelpers.ts
+++ packages/data-portal-commons/src/lib/clickhouseHelpers.ts
@@ -5,8 +5,11 @@ import { SelectedFilter } from '@htan/data-portal-filter';
 
 import { CountByType } from './types';
 
-export const DEFAULT_CLICKHOUSE_URL =
-    'https://mecgt250i0.us-east-1.aws.clickhouse.cloud:8443/htan_2025-09-09';
+export const DEFAULT_CLICKHOUSE_HOST =
+    'https://mecgt250i0.us-east-1.aws.clickhouse.cloud:8443';
+export const DEFAULT_CLICKHOUSE_DB = 'htan_2025_09_09';
+export const DEFAULT_CLICKHOUSE_URL = `${DEFAULT_CLICKHOUSE_HOST}/${DEFAULT_CLICKHOUSE_DB}`;
+
 const defaultClient: WebClickHouseClient = createClient({
     url: DEFAULT_CLICKHOUSE_URL,
     username: 'webuser',
